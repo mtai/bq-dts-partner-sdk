ab85154cd40e2d1751716f3f5ea6edc6db8ce50c
Metod N <metod@simpel.si>
2018-10-12 11:30:53 +0300

    include api_discovery.json and bin/data_source_definition.py in the installed package

--- setup.py
+++ setup.py
@@ -5,4 +5,6 @@ setup(name='bq-dts-partner-sdk',
       description='BigQuery Data Transfer Service - Integration SDK',
       url='https://github.com/mtai/bq-dts-partner-sdk/',
       packages=['bq_dts'],
+      package_data={'bq_dts': ['api_discovery.json']},
+      data_files=[('bin', ['bin/data_source_definition.py'])],
       )
